4d47412a986ca1fc028e4840c0655e8ca7f2f39f
Palendar <Palendar.ECE@gmail.com>
2017-01-04 17:03:37 +0100

    add search view

--- js/app.js
+++ js/app.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
   $('.message a').click(function(){
      $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
   });
+
+  $('#tabs').tabulous({
+    effect: 'scale'
+  });
 });
