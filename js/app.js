066497fb924e961c6a9851cda8ddf006c555cb5f
Palendar <Palendar.ECE@gmail.com>
2017-01-05 15:34:39 +0100

    modify search.html

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
+function getNumberGroupsFriendsEvents() {
+  var tabgroups = $('.tabgroups').text();
+  var numbergroups = $('#tabs-1 table tr').length;
+  $('.tabgroups').text(tabgroups + ' (' + numbergroups +')');
+
+  var tabfriends = $('.tabfriends').text();
+  var numberfriends = $('#tabs-2 table tr').length;
+  $('.tabfriends').text(tabfriends + ' (' + numberfriends +')');
+
+  var tabevents = $('.tabevents').text();
+  var numberevents = $('#tabs-3 table tr').length;
+  $('.tabevents').text(tabevents + ' (' + numberevents +')');
+
+}
+
 $(document).ready(function() {
   $('.message a').click(function(){
      $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
@@ -10,8 +25,11 @@ $(document).ready(function() {
   $('.settings-item').click(function () {
     $('#' + this.dataset.id).toggle();
   });
+
   $('#newGroup').click(function(){
     window.location.href = "http://localhost/PalendarFront/views/newGroup.html";
   });
+
+  getNumberGroupsFriendsEvents();
 
 });
