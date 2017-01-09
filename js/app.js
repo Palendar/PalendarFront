9fbce75e72e72ebcf56ab75bf2f61c3c0fecd561
Palendar <Palendar.ECE@gmail.com>
2017-01-09 16:13:26 +0100

    add numberfriends & group in contact.html

--- js/app.js
+++ js/app.js
@@ -1,8 +1,22 @@
+function getNumberFriendsGroups() {
+  var tabgroups = $('.tabgroups a').text();
+  var numbergroups = $('#mygroups table tr').length;
+  $('.tabgroups a').text(tabgroups + ' (' + numbergroups +')');
+
+  var tabfriends = $('.tabfriends a').text();
+  var numberfriends = $('#myfriends table tr').length;
+  $('.tabfriends a').text(tabfriends + ' (' + numberfriends +')');
+
+}
+
 function getMyPalendar() {
   window.location.href = "http://localhost/PalendarFront/views/mypalendar.html"
 }
 
 $(document).ready(function() {
+
+  getNumberFriendsGroups();
+
   $('.message a').click(function(){
      $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
   });
