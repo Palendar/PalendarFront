2a1857a3b1b218da970f0d0b34418c351dc1adb4
Palendar <Palendar.ECE@gmail.com>
2017-01-20 16:34:12 +0100

    register ok

--- js/login.js
+++ js/login.js
@@ -8,6 +8,10 @@ function getHome() {
 
 $(window).on('load', function () {
 
+  $('.message a').click(function(){
+     $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+  });
+
   $(".login-form").submit(function(event) {
     var email = $(this).closest('form').find('input').eq(0).val();
     var password = $(this).closest('form').find('input').eq(1).val();
@@ -21,6 +25,39 @@ $(window).on('load', function () {
         } else {
           $(".errorloginpassword").show();
           $(".login-form").closest('form').find('input').eq(1).val("");
+        }
+      }
+    }, "json");
+    event.preventDefault();
+  });
+
+  $(".register-form").submit(function(event) {
+    var fn = $(this).closest('form').find('input').eq(0).val();
+    var ln = $(this).closest('form').find('input').eq(1).val();
+    var email = $(this).closest('form').find('input').eq(2).val();
+    var password = $(this).closest('form').find('input').eq(3).val();
+
+    console.log(fn +' ' + ln + ' '+ email + ' ' + password);
+
+    var checkEmailUrl = 'http://vinci.aero/palendar/php/checkEmail.php';
+    var registerUrl = 'http://vinci.aero/palendar/php/register.php';
+    $.post(checkEmailUrl, {email: email}, function(data, status) {
+      if (status === "success") {
+        var emailOK = data.validate;
+        console.log("checkemail :" + emailOK);
+        if(emailOK) {
+          $.post(registerUrl, {firstname: fn, lastname: ln, email: email, password: password}, function(data, status) {
+            if (status === "success") {
+              var registerOk = data.validate;
+              if(registerOk) {
+                console.log("ok");
+              } else {
+                //error message email
+              }
+            }
+          }, "json");
+        } else {
+          //error message email
         }
       }
     }, "json");
